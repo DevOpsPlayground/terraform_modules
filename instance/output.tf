8a0a530d5ac046de4812a87b8e105d58aeeb2cb0
sirlegendary <wale141@yahoo.com>
2024-05-17 10:23:40 +0100

    Update output.tf

--- instance/output.tf
+++ instance/output.tf
@@ -2,6 +2,10 @@ output "public_ips" {
   value       = aws_instance.main.*.public_ip
   description = "The public ips of the workstation"
 }
+output "ids" {
+  value       = aws_instance.main.*.id
+  description = "The ids of the workstation"
+}
 output "unique_identifiers" {
   value = aws_instance.main.*.tags.Name
 }
